8f4dec75bcd9e27c20e864380c7d36c4712b37fc
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2016-10-15 00:03:40 +0300

    Index view renamed to CardNumber

--- Cash Machine/App_Start/RouteConfig.cs
+++ Cash Machine/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Cash_Machine
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "CardNumber", id = UrlParameter.Optional }
             );
         }
     }
--- Cash Machine/Controllers/HomeController.cs
+++ Cash Machine/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Cash_Machine.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult CardNumber()
         {
             return View();
         }
